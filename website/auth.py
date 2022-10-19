006976669e2000b46f576ae0421096de465547ce
renkwok <115599649+renkwok@users.noreply.github.com>
2022-10-19 13:26:59 +1000

    Login update

--- website/auth.py
+++ website/auth.py
@@ -11,28 +11,19 @@ from . import db
 #create a blueprint
 bp = Blueprint('auth', __name__)
 
+@bp.route('/login', methods=['GET','POST'])
+def login():
+    loginForm = LoginForm()
+    if loginForm.validate_on_submit():
+        print('Successfully logged in')
+        flash('You logged in successfully')
+        return redirect(url_for('auth.login'))
+    return render_template('user.html', form=loginForm,  heading='Login')
 
-# this is the hint for a login function
-# @bp.route('/login', methods=['GET', 'POST'])
-# def authenticate(): #view function
-#     print('In Login View function')
-#     login_form = LoginForm()
-#     error=None
-#     if(login_form.validate_on_submit()==True):
-#         user_name = login_form.user_name.data
-#         password = login_form.password.data
-#         u1 = User.query.filter_by(name=user_name).first()
-#         if u1 is None:
-#             error='Incorrect user name'
-#         elif not check_password_hash(u1.password_hash, password): # takes the hash and password
-#             error='Incorrect password'
-#         if error is None:
-#             login_user(u1)
-#             nextp = request.args.get('next') #this gives the url from where the login page was accessed
-#             print(nextp)
-#             if next is None or not nextp.startswith('/'):
-#                 return redirect(url_for('index'))
-#             return redirect(nextp)
-#         else:
-#             flash(error)
-#     return render_template('user.html', form=login_form, heading='Login')
+@bp.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print('Successfully registered')
+        return redirect(url_for('auth.login'))
+    return render_template('user.html', form=form)
