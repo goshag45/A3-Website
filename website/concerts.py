dcec1320c86c0e42cbe98b71eca96adb99ec178f
goshag45 <goshagugl44@gmail.com>
2022-10-22 19:20:53 +1000

    success! link from index to concert now works, CSS NOT WORKING YET

--- website/concerts.py
+++ website/concerts.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('concert', __name__, url_prefix='/details')
+bp = Blueprint('concert', __name__)
 
 @bp.route('/concert')
 def details():
-    return render_template('templates/concert.html')
+    return render_template('concerts/show.html')
