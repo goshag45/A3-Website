13b4e4af41007154ff9f3b938564a28e3fe9b0f3
goshag45 <goshagugl44@gmail.com>
2022-10-26 11:22:02 +1000

    fix

--- website/auth.py
+++ website/auth.py
@@ -12,7 +12,33 @@ from .models import User
 #create a blueprint
 bp = Blueprint('auth', __name__ )
 
-@bp.route('/login', methods=['GET','POST'])
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register = RegisterForm()
+    #the validation of form submis is fine
+    if (register.validate_on_submit() == True):
+            #get username, password and email from the form
+            uname =register.user_name.data
+            pwd = register.password.data
+            email=register.email_id.data
+            #check if a user exists
+            u1 = User.query.filter_by(name=uname).first()
+            if u1:
+                flash('User name already exists, please login')
+                return redirect(url_for('auth.login'))
+            # don't store the password - create password hash
+            pwd_hash = generate_password_hash(pwd)
+            #create a new user model object
+            new_user = User(name=uname, password_hash=pwd_hash, emailid=email)
+            db.session.add(new_user)
+            db.session.commit()
+            #commit to the database and redirect to HTML page
+            return redirect(url_for('main.index'))
+    #the else is called when there is a get message
+    else:
+        return render_template('user.html', form=register, heading='Register')
+
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
     error=None
@@ -35,10 +61,8 @@ def login():
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
 
-@bp.route('/register', methods=['GET','POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        print('Successfully registered')
-        return redirect(url_for('auth.login'))
-    return render_template('user.html', form=form)
+@bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'You have been logged out'
