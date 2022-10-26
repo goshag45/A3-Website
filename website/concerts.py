7c2891f9dc6232cd9cc5f5df36980a122369a04f
goshag45 <goshagugl44@gmail.com>
2022-10-26 10:33:09 +1000

    working on it

--- website/concerts.py
+++ website/concerts.py
@@ -1,8 +1,19 @@
 from flask import Blueprint, render_template
 from .models import Concert, Comment
+from flask_login import login_required, current_user
+import os
 
 bp = Blueprint('concert', __name__)
 
 @bp.route('/concert')
 def details():
     return render_template('concerts/show.html')
+
+def check_upload_file(form):
+    fp.form.image.data
+    filename=fp.filename
+    BASE_PATH=os.path.dirname(__file__)
+    upload_path=os.path.join(BASE_PATH, 'static/img', secure_filename(filename))
+    db_upload_path='/static/img' + secure_filename(filename)
+    fp_save(upload_path)
+    return db_upload_path
