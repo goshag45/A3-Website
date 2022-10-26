694527d054364951a6bf8386a447129a95a40c86
renkwok <115599649+renkwok@users.noreply.github.com>
2022-10-26 11:47:43 +1000

    new

--- website/concerts.py
+++ website/concerts.py
@@ -1,18 +1,69 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Concert, Comment
+from .forms import ConcertForm, CommentForm
+from flask_login import login_required, current_user
+from . import db
+from werkzeug.utils import secure_filename
 from flask_login import login_required, current_user
 import os
 
-bp = Blueprint('concert', __name__)
+bp = Blueprint('concert', __name__, url_prefix='/concerts')
 @bp.route('/concert')
-def details():
+def show(id):
+    concert = Concert.query.filter_by(id=id).first()
+    # create the comment form
+    cform = CommentForm() 
     return render_template('concerts/show.html')
 
+@bp.route('/create', methods = ['GET', 'POST'])
+@login_required
+def create():
+  print('Method type: ', request.method)
+  form = ConcertForm()
+  if form.validate_on_submit():
+    #call the function that checks and returns image
+    db_file_path=check_upload_file(form)
+    concert=Concert(name=form.name.data,description=form.description.data, 
+    image=db_file_path,currency=form.currency.data)
+    # add the object to the db session
+    db.session.add(concert)
+    # commit to the database
+    db.session.commit()
+    print('Successfully created new travel concert', 'success')
+    #Always end with redirect when form is valid
+    return redirect(url_for('concert.create'))
+  return render_template('concerts/create.html', form=form)
+
 def check_upload_file(form):
-    fp.form.image.data
-    filename=fp.filename
-    BASE_PATH=os.path.dirname(__file__)
-    upload_path=os.path.join(BASE_PATH, 'static/img', secure_filename(filename))
-    db_upload_path='/static/img' + secure_filename(filename)
-    fp_save(upload_path)
-    return db_upload_path
+  #get file data from form  
+  fp=form.image.data
+  filename=fp.filename
+  #get the current path of the module file… store image file relative to this path  
+  BASE_PATH=os.path.dirname(__file__)
+  #upload file location – directory of this file/static/image
+  upload_path=os.path.join(BASE_PATH,'static/image',secure_filename(filename))
+  #store relative path in DB as image location in HTML is relative
+  db_upload_path='/static/image/' + secure_filename(filename)
+  #save the file and return the db upload path  
+  fp.save(upload_path)
+  return db_upload_path
+
+@bp.route('/<concert>/comment', methods = ['GET', 'POST'])  
+@login_required
+def comment(concert):  
+    form = CommentForm()  
+    concert_obj = Concert.query.filter_by(id=concert).first()  
+    if form.validate_on_submit():  
+      #read the comment from the form
+      comment = Comment(text=form.text.data,  
+                        destination=concert_obj,
+                        user=current_user) 
+
+      db.session.add(comment) 
+      db.session.commit() 
+
+      #flashing a message which needs to be handled by the html
+      #flash('Your comment has been added', 'success')  
+      print('Your comment has been added', 'success') 
+    return redirect(url_for('concert.show', id=concert))
+    
