68a6211206844b8cb4037e3bc8dd0086781ea9e4
goshag45 <goshagugl44@gmail.com>
2022-11-01 13:50:49 +1000

    fixed up some datatypes

--- website/forms.py
+++ website/forms.py
@@ -13,7 +13,8 @@ class ConcertForm(FlaskForm):
   image = FileField('Destination Image', validators=[
     FileRequired(message='Image cannot be empty'),
     FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
-  date = StringField('Date', validators=[InputRequired()])
+  datetime = StringField('Date', validators=[InputRequired()])
+  address = StringField('Address', validators=[InputRequired()])
   submit = SubmitField("Create")
 
 #creates the login information
