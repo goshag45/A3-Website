bf566035e7378466479d077f00b7129f3de7e3a9
goshag45 <goshagugl44@gmail.com>
2022-11-01 19:05:58 +1000

    more form stuff

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, DateField, TimeField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed 
 
@@ -13,8 +13,8 @@ class ConcertForm(FlaskForm):
   image = FileField('Destination Image', validators=[
     FileRequired(message='Image cannot be empty'),
     FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
-  date = StringField('Date', validators=[InputRequired()])
-  time = StringField('Time', validators=[InputRequired()])
+  date = DateField('Date', validators=[InputRequired()])
+  time = TimeField('Time', validators=[InputRequired()])
   address = StringField('Address', validators=[InputRequired()])
   submit = SubmitField("Create Event")
 
