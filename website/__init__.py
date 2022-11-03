c35968377e700357fbd368fbebe998b80a0f9674
renkwok <115599649+renkwok@users.noreply.github.com>
2022-11-03 20:26:15 +1000

    new changes

--- website/__init__.py
+++ website/__init__.py
@@ -47,3 +47,7 @@ def create_app():
 @app.errorhandler(404) 
 def not_found(e): 
    return render_template("404.html"), 404
+
+@app.errorhandler(500) 
+def server_error(e): 
+   return render_template("500.html"), 500
