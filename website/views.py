487b676825a57d9b37d0ea583e7e50fa8f66c657
renkwok <115599649+renkwok@users.noreply.github.com>
2022-11-04 12:47:23 +1000

    new page for search function

--- website/views.py
+++ website/views.py
@@ -14,9 +14,9 @@ def index():
 def search():
     if request.args['search']:
         print(request.args['search'])
-        concert_search = "%" + request.args['search'] + '%'
-        concerts = Concert.query.filter(Concert.name.like(concert_search)).all()
-        return render_template('index.html', concerts=concerts)
+        artist_search = "%" + request.args['search'] + '%'
+        concerts = Concert.query.filter(Concert.name.like(artist_search)).all()
+        return render_template('concerts/search.html', concerts=concerts)
     else:
         concerts=Concert.query.all()
         return render_template('index.html', concerts=concerts)
