9c7273c121ef79e3ac5bc2bade04920a11eb0e67
renkwok <115599649+renkwok@users.noreply.github.com>
2022-11-04 12:09:06 +1000

    search function

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from . import db
 from .models import Concert
 
@@ -8,3 +8,14 @@ bp = Blueprint('main', __name__)
 def index():
     concerts=Concert.query.all()
     return render_template('index.html', concerts=concerts)
+
+
+@bp.route('/search')
+def search():
+    if request.args['search']:
+        print(request.args['search'])
+        concert_search = "%" + request.args['search'] + '%'
+        concerts = Concert.query.filter(Concert.name.like(concert_search)).all()
+        return render_template('index.html', concerts=concerts)
+    else:
+        return render_template('index.html')
