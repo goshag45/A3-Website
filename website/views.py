466ab5909b98a895b4ac97aa76d640c8d3faea84
renkwok <115599649+renkwok@users.noreply.github.com>
2022-11-04 20:36:52 +1000

    changes to view.py and index.html

--- website/views.py
+++ website/views.py
@@ -14,9 +14,13 @@ def index():
 def search():
     if request.args['search']:
         print(request.args['search'])
-        artist_search = "%" + request.args['search'] + '%'
-        concerts = Concert.query.filter(Concert.name.like(artist_search)).all()
-        return render_template('concerts/search.html', concerts=concerts)
+        print("I AM HERE HELLOOOOOOO")
+        search_query = "%" + request.args['search'] + '%'
+        concerts = Concert.query.filter(Concert.name.like(search_query)).all()
+        # Don't know what I'm doing here, just testing
+        concerts_genre = Concert.query.filter(Concert.genre.like(search_query)).all()
+        #concerts = Concert.query.filter(Concert.city.like(search_query)).all()
+        return render_template('concerts/search.html', concerts=concerts, concerts_genre=concerts_genre)
     else:
         concerts=Concert.query.all()
         return render_template('index.html', concerts=concerts)
