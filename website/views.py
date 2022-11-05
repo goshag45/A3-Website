6ce9d939dd6dd0210dbe0947e3c20f5fba8ecddb
Huiyu001 <n10937935@qut.edu.au>
2022-11-05 23:53:18 +1000

    sdhaskjd

--- website/views.py
+++ website/views.py
@@ -1,17 +1,20 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from . import db
 from .models import Concert
+from .forms import ConcertForm
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
+    form = ConcertForm()
     concerts=Concert.query.all()
-    return render_template('index.html', concerts=concerts)
+    return render_template('index.html', form=form, concerts=concerts)
 
 
 @bp.route('/search')
 def search():
+    form=ConcertForm()
     if request.args['search']:
         print(request.args['search'])
         print("I AM HERE HELLOOOOOOO")
@@ -25,9 +28,27 @@ def search():
         #concerts = Concert.query.filter(Concert.city.like(search_query)).all()
         #return render_template('concerts/search.html', concerts=concerts, concerts_genre=concerts_genre)
         return render_template('concerts/search.html', concerts=concerts)
+    elif request.args.get("genre_options"):
+        # Look into the database for all results
+        print("bbb")
+        searches = Concert.query.all()
+        search_query = request.args.get("genre_options")
+        concerts = Concert.query.filter(Concert.genre.like(search_query)).all()
+        print(search_query)
+        # Search the user input in all results
+        for search in searches:
+            # if there is anything matches with the user input, return it as results
+            if search == search_query:
+                
+                return render_template('concerts/search.html')
+            else:
+                print("i dont know what to do")
+            
+        return render_template('concerts/search.html', concerts=concerts)
     else:
+        print("aaa")
         concerts=Concert.query.all()
-        return render_template('index.html', concerts=concerts)
+        return render_template('index.html', form=form, concerts=concerts)
 
         
 # @bp.route('/search')
